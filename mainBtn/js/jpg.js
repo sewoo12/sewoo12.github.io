d7b0d2665d51563a88cbcdfb817b59e41b028af4
sewoo12 <anse123422@naver.com>
2022-05-31 13:43:16 +0900

    Version 2022-05-31

--- mainBtn/js/jpg.js
+++ mainBtn/js/jpg.js
@@ -1,4 +1,4 @@
-document.getElementById("img1").src = h.경복궁[0]
-document.getElementById("img2").src = h.경복궁[1]
-document.getElementById("img3").src = h.경복궁[2]
-document.getElementById("img4").src = h.경복궁[3]
+// document.getElementById("img1").src = h.경복궁[0]
+// document.getElementById("img2").src = h.경복궁[1]
+// document.getElementById("img3").src = h.경복궁[2]
+// document.getElementById("img4").src = h.경복궁[3]
